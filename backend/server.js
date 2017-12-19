b6fc3b6cc01f44a70cf649ccbc6bdd00d3b8048d
Jordan Van Ness <jordanwvn@gmail.com>
2017-12-19 11:00:25 -0800

    testing server connections, adding in query tags

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const express = require('express'); // does all the server stuff
-const bodyparser = require('body-parser').urlencoded({extended: true}); // dependency for express
+const bodyparser = require('body-parser'); // dependency for express
 const pg = require('pg'); // postgress database
-const superagent = require ('superagent'); // for performing backend AJAX calls 
+const superagent = require ('superagent'); // for performing backend AJAX calls
 
 const app = express(); // invoke express
 const PORT = process.env.PORT || 3000; // export PORT=###
@@ -15,44 +15,55 @@ client.connect(); // new pg connection
 client.on('error', err => console.error(err)); // send error message to server
 
 // app.use(cors()); // if cross origin becomes an issue
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static('../frontend'));
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3')); // test that we can load a basic page
+app.get('/', (req, res) => res.sendFile('index.html', {root: '../frontend'})); // test that we can load a basic page
+
+app.get('/api', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
 
 // example from book app
-app.get('/api/v1/books/find', (req, res) => {
-    
+app.get('/api/v1/questions/:language/:specialties', (req, res) => {
+
     /* const query = `https://www.googleapis.com/books/v1/volumes?`;
     // inauthor:frank%20herbert+intitle:dune+isbn:9780143111580
     superagent.get(`${query}`)
       .query('q' : req.body)
-      .end(function(err, res){})  
+      .end(function(err, res){})
       */
-    
-    let url = 'https://api.stackexchange.com/2/2/questions?pagesize=50&order=desc&sort=activity';
-    let query = ''
-    if(req.query.lang) query += `+lang:${req.query.lang}`;
-    if(req.query.terms) query += `+terms:${req.query.terms}`;
-    console.log('query-',query);
+
+
+  // {language: 'java', terms: 'item1; item2; item3'}
+  let url = 'https://api.stackexchange.com/2.2/';
+  let query = 'questions?order=desc&sort=activity';
+  query += `&tagged=${req.query.lang}`;
+  if(req.query.terms) query += `${req.query.terms}`;
+  query += `&site=stackoverflow&key=${API_KEY}`;
   
-    superagent.get(url)
-      .query({'q': query})
-      .query({'key': API_KEY})
-      .then(response => response.body.items.map((book, idx) => {
-        let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
-        let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
-  
-        return {
-          title: title ? title : 'No title available',
-          author: authors ? authors[0] : 'No authors available',
-          isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
-          image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
-          description: description ? description : 'No description available',
-          book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
-        }
-      }))
-      .then(arr => res.send(arr))
-      .catch(console.error)
-  })
+  console.log('query-',query);
+
+  superagent.get(url)
+    .query({'q': query})
+    .query({'key': API_KEY})
+    .then(response => response.body.items.map((book, idx) => {
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+        book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
+      }
+    }))
+    .then(arr => res.send(arr))
+    .catch(console.error)
+})
 
 app.get('/questions/lang=javascript&terms=regex', (req, res) => {
     client.query(`SELECT * FROM ourtable WHERE;`)
@@ -60,5 +71,5 @@ app.get('/questions/lang=javascript&terms=regex', (req, res) => {
       .catch(console.error);
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL)); // listen for all routes, send to homepage
+  app.get('*', (req, res) => res.redirect(CLIENT_URL)); // listen for all routes, send to homepage
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // send confirmation to node console
