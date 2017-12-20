001791b7dace4ac26aa6c0698c078701eb38b0fa
Jordan Van Ness <jordanwvn@gmail.com>
2017-12-20 10:21:04 -0800

    all of our base API routes have been completed

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.get('/api', function(req, res) {
 
 // return all questions defined by tags
 app.get('/api/v1/questions/', (req, res) => {
-  superagent.get(baseUrl += 'questions') // searching within questions
+  superagent.get(baseUrl + 'questions') // searching within questions
     .query({tagged: `${req.query.tags}`})
     .query({order: 'desc'})
     .query({sort: 'activity'})
@@ -61,9 +61,29 @@ app.get('/api/v1/questions/', (req, res) => {
     .catch(console.error)
 });
 
+// return the profile of a single user
+app.get('/api/v1/user/', (req, res) => {
+  superagent.get(baseUrl + 'users/:id')
+  .query({order: 'desc'})
+  .query({sort: 'reputation'})
+  .query({site: 'stackoverflow'})
+  .query({key: `${API_KEY}`})
+  .then (res => res.body.items.map((user, idx) => {
+    return {
+      user: user.display_name ? user.display_name : 'no user name',
+      link: user.link ? user.link: 'no user link',
+      user_image: user.profile_image ? user.profile_image : 'no user image',
+      reputation: user.reputation ? user.reputation : 'user reputation unavailable',
+      creation_date: user.creation_date ? user.creation_date : 'creation date unavailable',
+      last_access_date: user.last_access_date ? user.last_access_date : 'no last access date available',
+      location: user.location ? user.location : 'no user location'
+    }
+  }))
+});
+
 // return top users
 app.get('/api/v1/top-users/', (req, res) => {
-  superagent.get(baseUrl += 'users')
+  superagent.get(baseUrl + 'users')
   .query({order: 'desc'})
   .query({sort: `${req.query.sort}`}) // either location or reputation
   .query({site: 'stackoverflow'})
@@ -74,7 +94,31 @@ app.get('/api/v1/top-users/', (req, res) => {
       link: user.link ? user.link: 'no user link',
       user_image: user.profile_image ? user.profile_image : 'no user image',
       reputation: user.reputation ? user.reputation : 'user reputation unavailable',
+      top_answer_rate: user.accept_rate ? user.accept_rate : 'no acceptance rate available',
       location: user.location ? user.location : 'no user location'
+    }
+  }))
+  .then(arr => res.send(arr))
+  .catch(console.error)
+});
+
+// return unanswered questions for a specific tag
+app.get('/api/v1/unanswered-questions/', (req, res) => {
+  superagent.get(baseUrl + 'questions/unanswered')
+  .query({tagged: `${req.query.tags}`})
+  .query({order: 'desc'})
+  .query({sort: 'votes'})
+  .query({site: 'stackoverflow'})
+  .query({key: `${API_KEY}`})
+  .then (res => res.body.items.map((question, idx) => {
+    return {
+      title: question.title ? question.title : 'no question title',
+      link: question.link ? question.link: 'no question link',
+      user: question.owner.display_name ? question.owner.display_name : 'no username',
+      user_image: question.owner.profile_image ? question.owner.profile_image : 'no user image',
+      user_link: question.owner.link ? question.owner.link : 'no user link',
+      creation_date: question.creation_date ? question.creation_date : 'no date of creation',
+      view_count: question.view_count ? question.view_count : 'view count unavailable'
     }
   }))
   .then(arr => res.send(arr))
